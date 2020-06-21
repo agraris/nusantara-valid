de8ae5ce202aa030548b85b3aca968be3809a5b7
magicjar <9734293+magicjar@users.noreply.github.com>
2020-06-21 10:28:53 +0700

    Fix NIP regex

--- src/datas/nomorIndukPegawaiNegeriSipil.ts
+++ src/datas/nomorIndukPegawaiNegeriSipil.ts
@@ -7,6 +7,6 @@
  * NIP_REGEX[3] = Gender (1 for man, 2 for woman)
  * NIP_REGEX[4] = Index
 */
-export const NIP_REGEX = /([0-9]{8})([0-9]{6})([1,2]{1})([0-9]{3})/
+export const NIP_REGEX = /^([0-9]{8})([0-9]{6})([1,2]{1})([0-9]{3})$/
 
 export const NIP_LENGTH = 18
