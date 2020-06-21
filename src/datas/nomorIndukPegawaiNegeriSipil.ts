3b059573e013d53f51b625886d1e77bff51e18cc
magicjar <9734293+magicjar@users.noreply.github.com>
2020-06-21 10:52:06 +0700

    Correcting code header

--- src/datas/nomorIndukPegawaiNegeriSipil.ts
+++ src/datas/nomorIndukPegawaiNegeriSipil.ts
@@ -1,12 +1,12 @@
-/*
+/**
  * NIP_REGEX
  *
- * NIP_REGEX[0] = NIP
- * NIP_REGEX[1] = Birthday
- * NIP_REGEX[2] = Recruiting Years and Month
- * NIP_REGEX[3] = Gender (1 for man, 2 for woman)
- * NIP_REGEX[4] = Index
-*/
+ * NIP_REGEX[0] = NIP Number has 18 digits
+ * NIP_REGEX[1] = 8 digits of Birthday in YYYYMMDD format
+ * NIP_REGEX[2] = 6 digits of Recruiting day in YYYYMM format
+ * NIP_REGEX[3] = 1 digit of Gender identifier (1 for man, 2 for woman)
+ * NIP_REGEX[4] = 3 digits of Index
+**/
 export const NIP_REGEX = /^([0-9]{8})([0-9]{6})([1,2]{1})([0-9]{3})$/
 
 export const NIP_LENGTH = 18
