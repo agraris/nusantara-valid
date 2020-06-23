335469823b5aadc5012bcb15b935c8e57c8b910c
magicjar <9734293+magicjar@users.noreply.github.com>
2020-06-23 18:03:41 +0700

    Update to latest rollup-plugin-babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 
 export default {
     input: 'src/js/nusantara-valid.js',
@@ -19,7 +19,8 @@ export default {
     ],
     plugins: [
         babel({
-            exclude: 'node_modules/**'
+            exclude: 'node_modules/**',
+            babelHelpers: 'bundled'
         })
     ]
 }
