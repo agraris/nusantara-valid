2e89f13be11bc6a2e570588c6b6324f714fffd82
magicjar <9734293+magicjar@users.noreply.github.com>
2020-06-23 08:14:53 +0700

    Compile 3 different type (cjs, es, and umd)

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,18 @@
 export default {
     input: 'src/js/nusantara-valid.js',
-    output: {
-        file: 'dist/nusantara-valid.js',
-        format: 'umd',
-        name: 'NusantaraValid'
-    },
+    output: [
+        {
+            file: 'dist/nusantara-valid.cjs.js',
+            format: 'cjs',
+            name: 'NusantaraValid'
+        }, {
+            file: 'dist/nusantara-valid.es.mjs',
+            format: 'es',
+            name: 'NusantaraValid'
+        }, {
+            file: 'dist/nusantara-valid.umd.js',
+            format: 'umd',
+            name: 'NusantaraValid'
+        }
+    ],
 }
