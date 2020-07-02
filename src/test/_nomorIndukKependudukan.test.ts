87a0268a830b04e06506128344da9f30b7d05706
magicjar <9734293+magicjar@users.noreply.github.com>
2020-07-02 12:24:43 +0700

    Refactor isValidNIK into isValidNIK(nik) and isValidNIKWithComparison(nik, comparison)

--- src/test/_nomorIndukKependudukan.test.ts
+++ src/test/_nomorIndukKependudukan.test.ts
@@ -1,72 +1,104 @@
 import { expect } from "chai"
-import { isValidNIK as isValid } from "../ts/functions/index"
+import { isValidNIK, isValidNIKWithComparison } from "../ts/functions"
 import { NIK_LENGTH } from "../ts/datas/nik"
 
 describe('NIK', () => {
     it('cannot be empty', () => {
-        expect(isValid('')).to.be.false
+        expect(isValidNIK('')).to.be.false
     })
 
     it('cannot be null', () => {
-        expect(isValid(null as any)).to.be.false
+        expect(isValidNIK(null as any)).to.be.false
     })
 
     describe('type', () => {
         it('cannot be a boolean', () => {
-            expect(isValid(true as any)).to.be.false
-            expect(isValid(false as any)).to.be.false
+            expect(isValidNIK(true as any)).to.be.false
+            expect(isValidNIK(false as any)).to.be.false
         })
 
         it('cannot be a array', () => {
-            expect(isValid([] as any)).to.be.false
+            expect(isValidNIK([] as any)).to.be.false
         })
 
         it('cannot be a object', () => {
-            expect(isValid({} as any)).to.be.false
+            expect(isValidNIK({} as any)).to.be.false
         })
 
-        it('can be a number', () => {
-            expect(isValid(3404021011880001)).to.be.true
+        it('cannot be a number', () => {
+            expect(isValidNIK(3404021011880001 as any)).to.be.false
         })
 
         it('can be a string', () => {
-            expect(isValid('3404021011880001')).to.be.true
+            expect(isValidNIK('3404021011880001')).to.be.true
         })
     })
 
-    describe('is valid', () => {
+    describe('isValidNIK() = true', () => {
         it('if it has ' + NIK_LENGTH + ' digit', () => {
-            expect(isValid('3404021011880001')).to.be.true
-            expect(isValid(3101012012990002)).to.be.true
+            expect(isValidNIK('3404021011880001')).to.be.true
+            expect(isValidNIK('3101012012990002')).to.be.true
         })
 
         it('if it has valid Province code (BPS code)', () => {
-            expect(isValid('3404031011880001')).to.be.true // 34 => Yogyakarta
-            expect(isValid(3101012012990002)).to.be.true // 31 => Jakarta
+            expect(isValidNIK('3404031011880001')).to.be.true // 34 => Yogyakarta
+            expect(isValidNIK('3101012012990002')).to.be.true // 31 => Jakarta
         })
 
         it('if it has valid birthday', () => {
-            expect(isValid('3404021011880001')).to.be.true // 10-11-88 => 1988-11-10 -> Man
-            expect(isValid(3101012012990002)).to.be.true // 20-12-99 => 1999-12-20 -> Man
-            expect(isValid('3404025011880001')).to.be.true // 50-11-88 => 1988-11-(50-40) -> Woman
-            expect(isValid(3101016012990002)).to.be.true // 60-12-99 => 1999-12-(60-40) -> Woman
+            expect(isValidNIK('3404021011880001')).to.be.true // 10-11-88 => 1988-11-10 -> Man
+            expect(isValidNIK('3101012012990002')).to.be.true // 20-12-99 => 1999-12-20 -> Man
+            expect(isValidNIK('3404025011880001')).to.be.true // 50-11-88 => 1988-11-(50-40) -> Woman
+            expect(isValidNIK('3101016012990002')).to.be.true // 60-12-99 => 1999-12-(60-40) -> Woman
         })
     })
 
-    describe('is invalid', () => {
+    describe('isValidNIK() = false', () => {
         it('if it does not meet the total NIP length of ' + NIK_LENGTH + ' digit', () => {
-            expect(isValid('34040210118800001')).to.be.false // Have 17 digits
-            expect(isValid(310101201299002)).to.be.false // Have 15 digits
+            expect(isValidNIK('34040210118800001')).to.be.false // Have 17 digits
+            expect(isValidNIK('310101201299002')).to.be.false // Have 15 digits
         })
 
         it('if it has invalid/unknown Province code (BPS code)', () => {
-            expect(isValid('3704031011880001')).to.be.false // 37 => Invalid/Unknown
-            expect(isValid(2201012012990002)).to.be.false // 22 => Invalid/Unknown
+            expect(isValidNIK('3704031011880001')).to.be.false // 37 => Invalid/Unknown
+            expect(isValidNIK('2201012012990002')).to.be.false // 22 => Invalid/Unknown
         })
 
         it('if it has invalid birthday', () => {
-            expect(isValid('3404021013880001')).to.be.false // 10-13-88 => 1988-13-10
-            expect(isValid(3101013212990002)).to.be.false // 32-12-99 => 1999-12-32
+            expect(isValidNIK('3404021013880001')).to.be.false // 10-13-88 => 1988-13-10
+            expect(isValidNIK('3101013212990002')).to.be.false // 32-12-99 => 1999-12-32
+        })
+    })
+
+    describe('isValidNIKWithComparison() = true', () => {
+        it('if it has valid comparison with provinceKey and birthday', () => {
+            expect(isValidNIKWithComparison('3404021011880001', { provinceKey: 'YO', birthday: '101188' })).to.be.true // 10-11-88 => 1988-11-10 -> Man
+        })
+
+        it('if it has valid comparison with provinceKey only', () => {
+            expect(isValidNIKWithComparison('3101012012990002', { provinceKey: 'JK' })).to.be.true // 20-12-99 => 1999-12-20 -> Man
+        })
+
+        it('if it has valid comparison with birthday only', () => {
+            expect(isValidNIKWithComparison('3404025011880001', { birthday: '101188' })).to.be.true // 50-11-88 => 1988-11-(50-40) -> Woman
+        })
+
+        it('it still valid if the comparison is all empty', () => {
+            expect(isValidNIKWithComparison('3101016012990002', { provinceKey: '', birthday: '' })).to.be.true // 60-12-99 => 1999-12-(60-40) -> Woman
+        })
+    })
+
+    describe('isValidNIKWithComparison() = false', () => {
+        it('if it has unmatch NIK\'s bps code and PROVINCE_DATA[provinceKey].bpsCode but has same NIK\'s birthday and user provided birthday', () => {
+            expect(isValidNIKWithComparison('3404021011880001', { provinceKey: 'JK', birthday: '101188' })).to.be.false // BPS code JK = 31 compared NIK = 34
+        })
+
+        it('if it has unmatch comparison between NIK\'s bps code and PROVINCE_DATA[provinceKey].bpsCode', () => {
+            expect(isValidNIKWithComparison('3101012012990002', { provinceKey: 'YO' })).to.be.false // provinceKey 'YO' has bpsCode of 34 but NIK's bpsCode is 31
+        })
+
+        it('if it has unmatch comparison between NIK\'s birthday with user provided birthday', () => {
+            expect(isValidNIKWithComparison('3404025011880001', { birthday: '101199' })).to.be.false // User born in 99 but NIK's born in 88
         })
     })
 })
--- src/ts/functions/_nomorIndukKependudukan.ts
+++ src/ts/functions/_nomorIndukKependudukan.ts
@@ -9,18 +9,47 @@ class NomorIndukKependudukan implements IValid, IValidLength {
         (a, b) => a.concat((PROVINCES_DATA as any)[b].bpsCode), []
     ) as number[]
 
-    isValid(nik: string | number): boolean {
-        if (!nik) return false
+    isValid(nik: string, provinceKey: string = '', birthday: string = ''): boolean {
+        if (!nik || typeof nik !== 'string') return false
 
         const validNIK = NIK_REGEX.exec(numbersOnly(nik))
 
         if (!validNIK) return false
 
+        // Comparison
+        if (provinceKey || birthday)
+        {
+            let validProvince = true
+            let validBirthday = true
+
+            if (provinceKey) {
+                if ((PROVINCES_DATA as any)[provinceKey].bpsCode != validNIK[1]) {
+                    validProvince = false
+                }
+            }
+
+            if (birthday) {
+                let dd = validNIK[4].substr(0, 2)
+                const mmyy = validNIK[4].substr(2, 4)
+
+                if (Number(dd) > 40) // Check if it is a man of woman
+                    dd = (Number(dd) - 40).toString()
+
+                const cardBirthday = dd + '' + mmyy
+                
+                if (cardBirthday != birthday) {
+                    validBirthday = false
+                }
+            }
+
+            return this.isValidLength(validNIK[0].length) && validProvince && validBirthday
+        }
+
         return this.isValidLength(validNIK[0].length) && this.isValidProvince(parseInt(validNIK[1])) && this.isValidBirthday(validNIK[4])
     }
 
-    isValidLength(nip: number): boolean {
-        return nip == NIK_LENGTH
+    isValidLength(nipLength: number): boolean {
+        return nipLength == NIK_LENGTH
     }
 
     isValidProvince(bpsCode: number): boolean {
@@ -33,8 +62,8 @@ class NomorIndukKependudukan implements IValid, IValidLength {
         if (!newBirthday) return false
 
         let dd = parseInt(newBirthday[1])
-        let mm = parseInt(newBirthday[2])
-        let yy = parseInt(newBirthday[3])
+        const mm = parseInt(newBirthday[2])
+        const yy = parseInt(newBirthday[3])
 
         if (dd > 40) // Check if it is a man of woman
             dd = dd - 40
@@ -45,8 +74,33 @@ class NomorIndukKependudukan implements IValid, IValidLength {
     }
 }
 
-const nik = new NomorIndukKependudukan()
+const theNIK = new NomorIndukKependudukan()
 
-export function isValidNIK(param: string | number) {
-    return nik.isValid(param)
+/**
+ * NIK validation without any comparison with user provided data.
+ * 
+ * It check if the NIK's bps code is included / exist in VALID_BPSCODE, check if the NIK's has valid birthday
+ * and if the NIK has the correct length.
+ *
+ * @param {string} nik - The NIK, the one to be validated
+ * @return {boolean} Is valid or not
+**/
+export function isValidNIK(nik: string) {
+    return theNIK.isValid(nik)
+}
+
+/**
+ * NIK validation with comparison with user provided provinceKey and birthday data.
+ * 
+ * It will compare NIK's bps code (see: NIK_REGEX[1]) with bps code from PROVINCE_DATA[provinceKey].bpsCode
+ * and will compare NIK's birthday (see: NIK_REGEX[4] with user provided birthday)
+ *
+ * @param {string} nik - The NIK, the one to be validated
+ * @param {object} comparison - The data to compare with, provinceKey (key from PROVINCE_DATA) and birthday (in DDMMYY format)
+ * @return {boolean} Is valid or not
+**/
+export function isValidNIKWithComparison(nik: string, comparison: { provinceKey?: string, birthday?: string }) {
+    let { provinceKey = '', birthday = '' } = comparison
+
+    return theNIK.isValid(nik, provinceKey, birthday)
 }
