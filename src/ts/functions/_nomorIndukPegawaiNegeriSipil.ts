0745b71a9faf98f90aa5fb167adf90ae0be9fce2
magicjar <9734293+magicjar@users.noreply.github.com>
2020-07-04 09:42:44 +0700

    Use formatDate() helper to validate the date instead of writing it's own isValidDate() function

--- src/ts/functions/_nomorIndukPegawaiNegeriSipil.ts
+++ src/ts/functions/_nomorIndukPegawaiNegeriSipil.ts
@@ -1,6 +1,6 @@
 import { IValid } from "../interface"
 import { NIP_LENGTH, NIP_REGEX } from "../datas/nip"
-import { numbersOnly, correctLength } from "../helpers"
+import { numbersOnly, correctLength, formatDate } from "../helpers"
 
 class NomorIndukPegawaiNegeriSipil implements IValid {
 
@@ -11,20 +11,20 @@ class NomorIndukPegawaiNegeriSipil implements IValid {
 
         if(!validNIP) return false
         
-        return this.isValidDate(validNIP[1]) && this.isValidDate(validNIP[2]) && correctLength(0, validNIP[0].length, { minLength: NIP_LENGTH })
-    }
-
-    isValidDate(date: string): boolean {
-        const newDate = date.replace(/(\d{4})(\d{2})(?:(\d{2})?)/, "$1-$2-$3")
-
-        const formatedDate = new Date(newDate)
-
-        return !isNaN(formatedDate.getTime())
+        return !isNaN(formatDate(validNIP[1]).getTime()) && !isNaN(formatDate(validNIP[2]).getTime()) && correctLength(0, validNIP[0].length, { minLength: NIP_LENGTH })
     }
 }
 
-const nip = new NomorIndukPegawaiNegeriSipil()
+const theNIP = new NomorIndukPegawaiNegeriSipil()
 
-export function isValidNIP(param: string) {
-    return nip.isValid(param)
+/**
+ * NIP validation.
+ *
+ * Validate NIP by validating NIP's birthday, NIP's recruitment date, and NIP's length
+ *
+ * @param {string} nip - The NIP to be validated
+ * @return {boolean} Is valid or not
+**/
+export function isValidNIP(nip: string): boolean {
+    return theNIP.isValid(nip)
 }
--- src/ts/helpers/_formatDate.ts
+++ src/ts/helpers/_formatDate.ts
@@ -1,11 +1,15 @@
+import { numbersOnly } from './_numbersOnly'
+
 /**
  * Format date from string
  *
- * @param {string} date - The string date.
+ * @param {string} date - The string date in YYYYMMDD format
  * @return {Date} Formated date.
 **/
 export function formatDate(date: string): Date {
-    const newDate = date.replace(/(\d{4})(\d{2})(?:(\d{2})?)/, "$1-$2-$3")
+    const numOnly = numbersOnly(date)
+
+    const newDate = numOnly.replace(/(\d{4})(?:(\d{2})?)(?:(\d{2})?)/, "$1-$2-$3")
 
     const formatedDate = new Date(newDate)
 
