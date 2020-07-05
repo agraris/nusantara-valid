dd0afeeef3df58944d502d4484ad2db55f370599
magicjar <9734293+magicjar@users.noreply.github.com>
2020-07-05 08:29:11 +0700

    Add banner in rollup outputs

--- rollup.config.js
+++ rollup.config.js
@@ -2,36 +2,51 @@ import babel from '@rollup/plugin-babel'
 import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 
+const path = require('path')
+const pkg = require(path.resolve(__dirname, 'package.json'))
+const copyleft = `/*!
+  * Nusantara Valid v${pkg.version}
+  * Copyright 2020 - ${pkg.author.name} (${pkg.author.url})
+  * Contributors (https://github.com/magicjar/nusantara-valid/graphs/contributors)
+  * Licensed under MIT (https://github.com/magicjar/nusantara-valid/blob/master/LICENSE)
+  */`
+
 export default {
-    input: 'src/ts/index.ts',
+    input: path.resolve(__dirname, 'src/ts/index.ts'),
     output: [
         {
             file: 'dist/cjs/nusantara-valid.js',
             format: 'cjs',
+            banner: copyleft,
             name: 'NusantaraValid'
         }, {
             file: 'dist/esm/nusantara-valid.mjs',
-            format: 'es',
+            format: 'esm',
+            banner: copyleft,
             name: 'NusantaraValid'
         }, {
             file: 'dist/umd/nusantara-valid.js',
             format: 'umd',
+            banner: copyleft,
             name: 'NusantaraValid'
         }, {
             file: 'dist/cjs/nusantara-valid.min.js',
             format: 'cjs',
+            banner: copyleft,
             name: 'NusantaraValid',
-            plugins: [terser()]
+            plugins: terser()
         }, {
             file: 'dist/esm/nusantara-valid.min.mjs',
             format: 'esm',
+            banner: copyleft,
             name: 'NusantaraValid',
-            plugins: [terser()]
+            plugins: terser()
         }, {
             file: 'dist/umd/nusantara-valid.min.js',
             format: 'umd',
+            banner: copyleft,
             name: 'NusantaraValid',
-            plugins: [terser()]
+            plugins: terser()
         }
     ],
     plugins: [
