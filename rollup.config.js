268ba40a9b612a202a376215041fbf759fa84624
magicjar <9734293+magicjar@users.noreply.github.com>
2020-07-07 08:44:34 +0700

    Update rollup workflow

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import babel from '@rollup/plugin-babel'
 import typescript from '@rollup/plugin-typescript'
-import { terser } from 'rollup-plugin-terser'
 
 const path = require('path')
 const pkg = require(path.resolve(__dirname, 'package.json'))
+const format = process.env.FORMAT
 const copyleft = `/*!
   * Nusantara Valid v${pkg.version}
   * Copyright 2020 - ${pkg.author.name} (${pkg.author.url})
@@ -15,38 +15,10 @@ export default {
     input: path.resolve(__dirname, 'src/ts/index.ts'),
     output: [
         {
-            file: 'dist/cjs/nusantara-valid.js',
-            format: 'cjs',
+            file: `dist/${format}/nusantara-valid.js`,
+            format: format,
             banner: copyleft,
             name: 'NusantaraValid'
-        }, {
-            file: 'dist/esm/nusantara-valid.mjs',
-            format: 'esm',
-            banner: copyleft,
-            name: 'NusantaraValid'
-        }, {
-            file: 'dist/umd/nusantara-valid.js',
-            format: 'umd',
-            banner: copyleft,
-            name: 'NusantaraValid'
-        }, {
-            file: 'dist/cjs/nusantara-valid.min.js',
-            format: 'cjs',
-            banner: copyleft,
-            name: 'NusantaraValid',
-            plugins: terser()
-        }, {
-            file: 'dist/esm/nusantara-valid.min.mjs',
-            format: 'esm',
-            banner: copyleft,
-            name: 'NusantaraValid',
-            plugins: terser()
-        }, {
-            file: 'dist/umd/nusantara-valid.min.js',
-            format: 'umd',
-            banner: copyleft,
-            name: 'NusantaraValid',
-            plugins: terser()
         }
     ],
     plugins: [
