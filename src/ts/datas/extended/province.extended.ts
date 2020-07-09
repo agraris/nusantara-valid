62bef2e85c13eb8584f1ec0d517a5dfceccb5616
magicjar <9734293+magicjar@users.noreply.github.com>
2020-07-09 09:25:45 +0700

    Extended data proposal

---
+++ src/ts/datas/extended/province.extended.ts
@@ -0,0 +1,205 @@
+/**
+ * PROVINCE_EXTENDED_DATA
+ *
+ * List of extended province data of Indonesia like distric in a province and sub-district in a district
+**/
+export const PROVINCE_EXTENDED_DATA =
+{
+    AC: {
+        district: {
+            
+        }
+    },
+    BA: {
+        district: {
+            
+        }
+    },
+    BB: {
+        district: {
+            
+        }
+    },
+    BE: {
+        district: {
+            
+        }
+    },
+    BT: {
+        district: {
+            
+        }
+    },
+    GO: {
+        district: {
+            
+        }
+    },
+    JA: {
+        district: {
+            
+        }
+    },
+    JB: {
+        district: {
+            
+        }
+    },
+    JI: {
+        district: {
+            
+        }
+    },
+    JK: {
+        district: {
+            
+        }
+    },
+    JT: {
+        district: {
+            
+        }
+    },
+    KB: {
+        district: {
+            
+        }
+    },
+    KI: {
+        district: {
+            
+        }
+    },
+    KR: {
+        district: {
+            
+        }
+    },
+    KS: {
+        district: {
+            
+        }
+    },
+    KT: {
+        district: {
+            
+        }
+    },
+    KU: {
+        district: {
+            
+        }
+    },
+    LA: {
+        district: {
+            
+        }
+    },
+    MA: {
+        district: {
+            
+        }
+    },
+    MU: {
+        district: {
+            
+        }
+    },
+    NB: {
+        district: {
+            
+        }
+    },
+    NT: {
+        district: {
+            
+        }
+    },
+    PA: {
+        district: {
+            
+        }
+    },
+    PB: {
+        district: {
+            
+        }
+    },
+    RI: {
+        district: {
+            
+        }
+    },
+    SA: {
+        district: {
+            
+        }
+    },
+    SB: {
+        district: {
+            
+        }
+    },
+    SG: {
+        district: {
+            
+        }
+    },
+    SN: {
+        district: {
+            
+        }
+    },
+    SR: {
+        district: {
+            
+        }
+    },
+    SS: {
+        district: {
+            
+        }
+    },
+    ST: {
+        district: {
+            
+        }
+    },
+    SU: {
+        district: {
+
+        }
+    },
+    YO: {
+        district: {
+            YO01: {
+                name: 'Kota Yogyakarta',
+                bpsCode: 1,
+            },
+            YO02: {
+                name: 'Kabupaten',
+                bpsCode: 2,
+            },
+            YO03: {
+                name: 'Kabupaten',
+                bpsCode: 3,
+            },
+            YO04: {
+                name: 'Kabupaten Sleman',
+                bpsCode: 4,
+                sub_district: {
+                    YO0401: {
+                        name: 'Kecamatan'
+                    },
+                    YO0402: {
+                        name: 'Kecamatan Godean'
+                    }
+                }
+            },
+            YO05: {
+                name: 'Kabupaten',
+                bpsCode: 5,
+            }
+        }
+    },
+};
